7618d70f2a2aaaa7ae2ba9e33161a969802ccbb7
Muhammed Yusayr Khan <yusayrk1@gmail.com>
2024-12-15 13:14:43 +0530

    hashing using bcrypt

---
+++ hashing/backend2/controllers/userController.js
@@ -0,0 +1,42 @@
+const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d'})
+}
+
+// loging user in
+const loginUser = async (req, res) => {
+    
+    const {email, password} = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        // create a token 
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
+    }
+    catch(err) {
+        res.status(400).json({err: err.message})
+    }
+}
+
+//signup user
+const signupUser = async function(req, res) {
+    const { email, password } = req.body
+    
+    try {
+        const user = await User.signup(email, password)
+
+        // create a token 
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
+    }
+    catch(err) {
+        res.status(400).json({err: err.message})
+    }
+
+}
+
+module.exports = { signupUser, loginUser };
